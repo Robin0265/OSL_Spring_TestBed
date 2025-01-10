5b6e9f7e3273082c51fbfd3cca774e03db6cb99b
Yuanshao Yang <yuanshao@umich.edu>
2025-01-10 15:44:31 +0000

    updated series spring test script

--- series_spring_test.py
+++ series_spring_test.py
@@ -1,10 +1,11 @@
 import sys
 import opensourceleg.tools.units as units
 from opensourceleg.osl import OpenSourceLeg
-from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder, Quality
+# from picamera2 import Picamera2, Preview
+# from picamera2.encoders import H264Encoder, Quality
 from libcamera import controls
 import numpy as np
+import time
 
 # Redirect to current folder
 sys.path.append("./")
@@ -17,7 +18,7 @@ GR_TRANS = 75/11
 GR_BOSTONGEAR = 50                  # Gear Ratio from Boston Gear
 
 
-T = 25                              # Period Time
+T = 10                              # Period Time
 
 TIME_TO_STEP = 1.0
 FREQUENCY = 200
@@ -27,60 +28,58 @@ k = np.pi/(48*50)
 
 if __name__ == '__main__': 
     osl = OpenSourceLeg(frequency=200, file_name="osl")
-    osl.add_joint(name="knee", port = "/dev/ttyACM0", gear_ratio=GR_ACTPACK*GR_BOSTONGEAR)
-    osl.log.add_attributes(osl, ["timestamp"])
-    osl.log.add_attributes(osl.knee, ["output_position"])
-    osl.log.add_attributes(locals(), ["abs_comp_angle","abs_angle","joint_angle"])
-    picam2 = Picamera2()
-    picam2.configure(picam2.create_video_configuration(main={"size": (1600, 1200)}))
-    picam2.set_controls({"FrameRate": 30})
+    osl.add_joint(name="knee", port = "/dev/ttyACM0", gear_ratio=GR_ACTPACK*GR_BOSTONGEAR, dephy_log=True)
+    osl.log.add_attributes(osl, ["timestamp", "_frequency"])
+    log_info = ["output_position", "output_velocity", "accelx", 
+                "motor_voltage", "motor_current", "battery_voltage", 
+                "battery_current"]
+    osl.log.add_attributes(osl.knee, log_info)
+
+    # picam2 = Picamera2()
+    # picam2.configure(picam2.create_video_configuration(main={"size": (1600, 1200)}))
+    # picam2.set_controls({"FrameRate": 30})
     # picam2.set_controls({"AfMode": controls.AfModeEnum.Auto})
-    encoder = H264Encoder()
-    try: 
+    # encoder = H264Encoder()
+
+    with osl: 
         # START RECORDING
-        picam2.start_preview(Preview.DRM)
-        picam2.start_recording(encoder, 'test.h264', quality=Quality.HIGH)
+        # picam2.start_preview(Preview.DRM)
+        # picam2.start_recording(encoder, 'test.h264', quality=Quality.HIGH)
         # TODO: ADD FUTEK SENSOR
         # ACTPACK TEST BEGINS
+
+
         osl.knee.start()
         osl.knee.set_mode(osl.knee.control_modes.position)
-
+        osl.knee.set_position_gains(
+            kp = 150, 
+            ki = 150, 
+            kd = 100, 
+            ff = 0,
+        )
         osl.knee.update()
-        pos = osl.knee.output_position
-        init_pos = pos
+        init_pos = osl.knee.output_position
+        osl.knee.set_output_position(position=init_pos)
+        # init_pos = pos
         i = 0
+        init_time = osl.clock.time()
+        T = osl.clock.time_since()
+        t_0 = 25
+        A = 2*np.pi
         for t in osl.clock:
-            
-            pos = pos + k
-            
-            osl.knee.set_position_gains(
-                # kp=200, 
-                # ki=200, 
-                # kd=50, 
-            )
-            osl.knee.set_output_position(position=pos)
-            osl.knee.update()
-            current_pos = osl.knee.output_position
-            
-            # osl.log.info(
-            #     units.convert_from_default(osl.knee.output_position, units.position.rad)
-            # )
-            print("####")
-            print(current_pos)
-            print("----")
-            print(init_pos)
-            
-            # print(current_pos)
-            if (current_pos - init_pos)>np.pi/2:
-                k = -np.pi/(48*50)
-                i = i + 1
-            elif (current_pos - init_pos)<-np.pi/2:
-                k = np.pi/(48*50)
-                i = i + 1
-            if ((int(i/50)==2) & (abs(current_pos - init_pos)<1e-1)):
+            osl.update()
+            if t < t_0:
+                position_command = A/t_0*t
+            elif t < 3*t_0:
+                position_command = -A/t_0*t + 2*A
+            elif t < 4*t_0:
+                position_command = A/t_0*t - 4*A
+            else:
                 break
 
+            position_command = position_command + init_pos
+            osl.knee.set_output_position(position=position_command)
+            osl.log.update()
             
-    finally: 
-        osl.knee.stop()
-        picam2.stop_recording()
+    # finally: 
+        # picam2.stop_recording()
